165eb5b95ef36f77cdc36c785fee6826692ac1e3
GalaxyBuilder-Oss <galaxybuilder.oss@gmail.com>
2025-05-29 07:55:26 +0700

    fix error params with NextRequest

--- app/api/json/[id]/route.ts
+++ app/api/json/[id]/route.ts
@@ -4,30 +4,38 @@ import {NextResponse, NextRequest} from 'next/server';
 const BASE_URL = process.env.MOCKUP_API_URL as string;
 
 export async function GET(
-    request: NextRequest,
-    context: { params: { id: string } }
+    request: NextRequest
 ) {
+    const url = new URL(request.url);
+    const parts = url.pathname.split("/"); // ['','api','json','123']
+    const id = parts[parts.length - 1];
     try {
-        const res = await axios.get(`${BASE_URL}/${context.params.id}`);
+        const res = await axios.get(`${BASE_URL}/${id}`);
         return NextResponse.json(res.data);
     } catch (e) {
         return NextResponse.json({error: 'Failed to fetch data by ID', detail: e});
     }
 }
 
-export async function PUT(request: NextRequest, context: { params: { id: string } }) {
+export async function PUT(request: NextRequest) {
+    const url = new URL(request.url);
+    const parts = url.pathname.split("/"); // ['','api','json','123']
+    const id = parts[parts.length - 1];
     try {
         const body = await request.json();
-        const res = await axios.put(`${BASE_URL}/${context.params.id}`, body);
+        const res = await axios.put(`${BASE_URL}/${id}`, body);
         return NextResponse.json(res.data);
     } catch (e) {
         return NextResponse.json({error: 'Failed to update data', detail: e});
     }
 }
 
-export async function DELETE(_: NextRequest, context: { params: { id: string } }) {
+export async function DELETE(request: NextRequest) {
+    const url = new URL(request.url);
+    const parts = url.pathname.split("/"); // ['','api','json','123']
+    const id = parts[parts.length - 1];
     try {
-        const res = await axios.delete(`${BASE_URL}/${context.params.id}`);
+        const res = await axios.delete(`${BASE_URL}/${id}`);
         return NextResponse.json({success: true, deleted: res.data});
     } catch (e) {
         return NextResponse.json({error: 'Failed to delete data', detail: e});
